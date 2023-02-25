557c8ad4ec677534620e71c8c26a6c0d81726746
bvanbreugel <boris-van-breugel@live.nl>
2023-02-25 16:24:10 +0000

    linting

--- src/domias/evaluator.py
+++ src/domias/evaluator.py
@@ -289,15 +289,11 @@ def evaluate_performance(
         # eqn2: \prop P_G(x_i)/P_X(x_i)
         # DOMIAS (BNAF for p_R estimation)
         if density_estimator == "bnaf":
-            p_R_evaluated = (
-                np.exp(
-                    compute_log_p_x(
-                        p_R_model, torch.as_tensor(X_test).float().to(device)
-                    )
-                    .cpu()
-                    .detach()
-                    .numpy()
-                )
+            p_R_evaluated = np.exp(
+                compute_log_p_x(p_R_model, torch.as_tensor(X_test).float().to(device))
+                .cpu()
+                .detach()
+                .numpy()
             )
 
         # DOMIAS (KDE for p_R estimation)
